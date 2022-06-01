b8807bf4395e245c51e31cbf0630f25912936834
Mohamed Fouad <mohamed.foaad@atpvital.com>
2022-06-01 18:44:02 +0200

    first commit

---
+++ App.tsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import {
+  SafeAreaView, 
+  Text,
+  useColorScheme
+} from 'react-native';
+import {
+  Colors
+} from 'react-native/Libraries/NewAppScreen';
+
+
+
+const App = () => {
+  const isDarkMode = useColorScheme() === 'dark';
+
+  const backgroundStyle = {
+    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+  };
+
+  return (
+    <SafeAreaView style={backgroundStyle}>
+    
+     <Text>
+       hi
+     </Text>
+    </SafeAreaView>
+  );
+};
+
+
+export default App;
